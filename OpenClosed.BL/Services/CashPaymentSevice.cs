918692cb3a084a5ac89c2528088fe3b2858964d2
A-Finger <anthony.palchyk@gmail.com>
2023-06-12 10:28:51 +0300

    Added CashPaymentSevice

---
+++ OpenClosed.BL/Services/CashPaymentSevice.cs
@@ -0,0 +1,14 @@
+﻿using OpenClosed.BL.Services.Interfaces;
+
+namespace OpenClosed.BL.Services
+{
+	public class CashPaymentSevice : IPaymentService
+	{
+		public void ProcessPayment(decimal amount)
+		{
+			// Some logic
+
+			Console.WriteLine($"Payment by cash: ${amount}");
+		}
+	}
+}
