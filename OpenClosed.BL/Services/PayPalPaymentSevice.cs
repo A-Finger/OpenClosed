e40a1069d688342fbea09ad7ba510ec418897e6d
A-Finger <anthony.palchyk@gmail.com>
2023-06-12 10:27:15 +0300

    Added PayPalPaymentSevice

---
+++ OpenClosed.BL/Services/PayPalPaymentSevice.cs
@@ -0,0 +1,14 @@
+﻿using OpenClosed.BL.Services.Interfaces;
+
+namespace OpenClosed.BL.Services
+{
+	public class PayPalPaymentSevice : IPaymentService
+	{
+		public void ProcessPayment(decimal amount)
+		{
+			// Some logic
+
+			Console.WriteLine($"Payment by PayPal: ${amount}");
+		}
+	}
+}
